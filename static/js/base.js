c2da80f7289f7b7f00b3b3e9d9abd3eccabb5786
BXS-Archipelago <bxs@tutanota.com>
2021-07-18 20:17:20 +0000

    Buttons, Headers, Text on Small Screens. Video and Switch in JS. Ch.5

--- static/js/base.js
+++ static/js/base.js
@@ -15,3 +15,22 @@ closeBtn.addEventListener('click', function() {
     sidebar.classList.remove('show-sidebar');
     
 })
+
+
+
+//Landing Page Video and Switch Button
+
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btn.addEventListener('click', function(){
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide');
+        video.pause();
+    }
+    else{
+        btn.classList.remove("slide");
+        video.play();
+    }
+    console.log("hello")
+})
