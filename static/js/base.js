fdbea42be50166f51cea7e39ab0749360b1dbec0
BXS-Archipelago <bxs@tutanota.com>
2021-08-04 15:10:49 +0000

    Update CSS for Banners and Icons Responsiveness. Ch.7

--- static/js/base.js
+++ static/js/base.js
@@ -34,3 +34,62 @@ btn.addEventListener('click', function(){
     }
     
 })
+
+// JS for Free Deliver Banner 
+// Rotating Text Credit to "Alphardex" at  
+// https://codepen.io/alphardex/pen/WNNVJeZ // 
+
+var values = document.getElementsByClassName('value');
+var valueArray = [];
+var currentValue = 0;
+
+values[currentValue].style.opacity = 1;
+for (var i = 0; i < values.length; i++) {
+  splitLetters(values[i]);
+}
+
+function changeValue() {
+  var cv = valueArray[currentValue];
+  var nv = currentValue === values.length-1 ? valueArray[0] : valueArray[currentValue + 1];
+  for (var i = 0; i < cv.length; i++) {
+    animateLetterOut(cv, i);
+  }
+  
+  for (var i = 0; i < nv.length; i++) {
+    nv[i].className = 'letter behind';
+    nv[0].parentElement.style.opacity = 1;
+    animateLetterIn(nv, i);
+  }
+  
+  currentValue = (currentValue == valueArray.length-1) ? 0 : currentValue+1;
+}
+
+function animateLetterOut(cv, i) {
+  setTimeout(function() {
+		cv[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nv, i) {
+  setTimeout(function() {
+		nv[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(value) {
+  var content = value.innerHTML;
+  value.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    value.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  valueArray.push(letters);
+}
+
+changeValue();
+setInterval(changeValue, 4000);
