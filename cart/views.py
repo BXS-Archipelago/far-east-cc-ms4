4b9d591fd64019149b2fe7eb62cf295c16e48f78
BXS-Archipelago <bxs@tutanota.com>
2021-08-10 09:48:09 +0000

    Create Add-to-Cart in product_detail, create view. Ch.15

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -8,3 +8,21 @@ def view_cart(request):
     """ A view to render the Cart contents page """
 
     return render(request, 'cart/cart.html')
+
+# submit Form incl ID and quantity, check if cart variable exists (create if 
+#  not), add to cart or update quantity if already there. 
+
+def add_to_cart(request, item_id):
+    """ this adds the quantity of the specified product to the Cart"""
+
+    quantity = int(request.POST.get('quantity)'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] +=quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
