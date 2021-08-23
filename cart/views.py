8ff12871c97c53ace1047c22b6d4d2271aaccb26
BXS-Archipelago <bxs@tutanota.com>
2021-08-23 15:13:58 +0000

    create checkout, add models. Ch.21

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from products.models import Product
 from django.contrib import messages
 # Create your views here.
@@ -16,7 +16,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ this adds the quantity of the specified product to the Cart"""
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -27,13 +27,12 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
-    
+        messages.success(request, f'Updated {product.name} to {cart[item_id]} items. Decisive!')
+        
     else:
         cart[item_id] = quantity
         messages.success(request, f'You added {product.name} to the cart. Nice!')
-       
-    # why doesn't cart[item_id] become quantity? 
-    
+               
     request.session['cart'] = cart
   
     return redirect(redirect_url)
@@ -41,15 +40,16 @@ def add_to_cart(request, item_id):
 # View to update product quantities or remove the item from Cart
 
 def adjust_cart(request, item_id):
-      
+    product = get_object_or_404(Product,pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} to {cart[item_id]} items. Decisive!')
     else:
         cart.pop(item_id)
-
+        messages.success(request, f'Removed {product.name} from your items. If you need any product information, just let us know!')
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
@@ -57,10 +57,11 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
       
     try:    
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)            
         request.session['cart'] = cart
-
+        messages.success(request, f'Removed {product.name} from your items. If you need any product information, just let us know!')
         return HttpResponse(status=200)
     except Exception as e:
         return HttpResponse(status=500)
---
+++ checkout/models.py
@@ -0,0 +1,52 @@
+# UUID generates the Order number
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
+
+#  Models will create and track orders for anyone who makes a purchase
+
+class Order(models.Model):
+    # Each order number is unique so editable = False here
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+
+    # automatically set date and time when a new order is placed. 
+
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+    # When user checks out, this uses the info in the payment form to create
+    # an order instance, then iterates through the items in the shopping cart
+    # creating a line item for each item.
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)    
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
