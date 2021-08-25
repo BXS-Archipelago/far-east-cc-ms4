2ce2bda8bafb92d899df02bf1a9b842ad5e24460
BXS-Archipelago <bxs@tutanota.com>
2021-08-25 14:50:07 +0000

    Added stripe for checkout functionality. Ch.24

--- checkout/views.py
+++ checkout/views.py
@@ -31,8 +31,6 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
- 
-
     order_form = OrderForm()
 
     if not stripe_public_key:
@@ -42,7 +40,7 @@ def checkout(request):
     context = {
         'order_form': order_form,   
         'stripe_public_key': stripe_public_key,
-        'client_secret': client_secret,     
+        'client_secret': intent.client_secret,     
     }
 
     return render(request, template, context)
