829a83aeaf692874e7c1e467f0a0a2c9b3bdef82
BXS-Archipelago <bxs@tutanota.com>
2021-09-01 17:15:22 +0000

    Create Order History Table for Profile.Ch.30

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from .models import UserProfile
 
 from .forms import UserProfileForm
 
+from checkout.models import UserProfileForm 
+
 def profile(request):
 #    Show the User's Profile
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -28,4 +30,18 @@ def profile(request):
 
     return render(request, template, context)
 
-    
+    def order_history(request, order_number):
+        order = get_object_or_404(order, order_number=order_number)
+
+        messages.info(request, (
+            f'this is a past confirmation for order number {order_number}.'
+            'A confirmation email was sent on the order date'
+        ))
+
+        template = 'checkout/checkout_success.html'
+        context = {
+            'order': order,
+            'from_profile': True
+        }
+
+        return render(request, template, context)
