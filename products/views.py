0c8f8a49ae8b9f887b8f5b7dc879a5deb531c529
BXS-Archipelago <bxs@tutanota.com>
2021-09-03 11:11:20 +0000

    Create product view, url and template. Ch.34

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q 
 from .models import Product, Category
 from django.db.models.functions import Lower
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -76,3 +77,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    # Allow new products to be added to the store
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = { 
+        'form': form
+    }
+
+    return render(request, template, context)
