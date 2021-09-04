128ea85a0a0eb6e7ed8a72630bd145b024ec4657
BXS-Archipelago <bxs@tutanota.com>
2021-09-04 15:42:02 +0000

    initiate deployment installs, requirements, migrate. Ch.39

--- far_east_cc/settings.py
+++ far_east_cc/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os 
+import dj_database_url 
 
 from pathlib import Path
 
@@ -131,7 +132,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
