31cb3b3ea3ed2c52a8188d0abd75a313c71eff76
BXS-Archipelago <bxs@tutanota.com>
2021-09-06 19:13:29 +0000

    Pushing to heroku to find bug

--- far_east_cc/settings.py
+++ far_east_cc/settings.py
@@ -181,9 +181,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] 
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static),)  
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static),)  
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
