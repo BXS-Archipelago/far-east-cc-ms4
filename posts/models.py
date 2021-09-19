ca48ef7b387411ae29bca21a36267a1e69cdc62e
BXS-Archipelago <bxs@tutanota.com>
2021-09-19 09:33:42 +0000

    update posts model for previous/next blog. Ch. 54

--- posts/models.py
+++ posts/models.py
@@ -30,7 +30,8 @@ class Post(models.Model):
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
     content = HTMLField()
-    
+    previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.title
