04720528484115aa746a68312a235d20061769b3
BXS-Archipelago <bxs@tutanota.com>
2021-09-23 12:09:02 +0000

    update view for contact form

--- home/views.py
+++ home/views.py
@@ -28,20 +28,19 @@ def index(request):
 
 def contact(request):
     if request.method == "POST":
-        message_subject = request.POST['message-subject']
+        
         message_name = request.POST['message-name']
         message_email = request.POST['message-email']
         message = request.POST['message']
 
         # sending email!
         send_mail(     
-            message_subject,       
+                   
             message_name,   
             message_email,          
-            message,            
-                        
-            # receiving email address
-            ['bxs@tutanota.com'],            
+            message,        
+            ['bxs@tutanota.com',]
+                       
         )
         context = {
             'message_name':message_name 
